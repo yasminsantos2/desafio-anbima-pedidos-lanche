21f39d3d9fdccc47c359aba9467fa1773e2622db
yasminsantos2 <yasminsantos.eng@gmail.com>
2026-04-15 14:38:38 -0300

    chore: inicializa projeto

---
+++ src/main/java/com/anbima/lanches/LanchesApplication.java
@@ -0,0 +1,13 @@
+package com.anbima.lanches;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LanchesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LanchesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/anbima/lanches/LanchesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.anbima.lanches;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LanchesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
